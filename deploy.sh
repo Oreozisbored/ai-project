d8c4dd2c51a21cf43d09543b8e7ea4224ac5fc8d
Oreozisbored <dogporkcow@gmail.com>
2024-06-02 18:43:01 -0400

    ew

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Oreozisbored/Oreozisbored.github.io.git main
+git push -f git@github.com:Oreozisbored/ai-project.git main:gh-pages
 
 cd -
